181c887c96c12737b178b2646c936d3e48963f30
Ilina Alina <ilinaalina>
2024-05-17 22:59:32 +0400

    Исправления, чтобы начиналось с (0, 1, ...)

--- 1.1.cpp
+++ 1.1.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 int fib(int n) {
-    if (n <= 2) {
-        return 1;
-    }
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
     return fib(n - 1) + fib(n - 2);
 }
 int main() {
     int n; cout << "n = ";  cin >> n;
-    int a = fib(n);
+    int a = fib(n - 1);
     cout << "fib = " << a << endl;
     return 0;
 }
