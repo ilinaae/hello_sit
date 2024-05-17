934975a72109f320e3412a33807c42497bc7084f
Ilina Alina <ilinaalina>
2024-05-17 22:53:30 +0400

    Добавление функции для рекурсивного вычисления числа Фибоначчи, начиная с (1, 1, ...)

--- 1.1.cpp
+++ 1.1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+int fib(int n) {
+    if (n <= 2) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
 int main() {
-    cout << "Hello, World!";
+    int n; cout << "n = ";  cin >> n;
+    int a = fib(n);
+    cout << "fib = " << a << endl;
     return 0;
 }
